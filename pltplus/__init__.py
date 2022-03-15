a202d0aecde35f01bdcd4fe1a35ece9bb39f6d63
Alexander Hagen <alexhagen6@gmail.com>
2022-03-14 21:23:41 -0400

    abstracted publish

--- pltplus/__init__.py
+++ pltplus/__init__.py
@@ -3,3 +3,7 @@ from matplotlib.pyplot import *
 def set_size(size):
     """Set the current default figure size."""
     rcParams["figure.figsize"] = size
+    
+    
+def publish(filename, fig=None):
+    all_artists.append(fig.get_children())
